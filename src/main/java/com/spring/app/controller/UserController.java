9be21c0935c733700f051670d4686583eb26b144
mamiprogramming <mamiprogramming@gmail.com>
2025-06-01 12:37:28 +0900

    UserController.javaの作成

---
+++ src/main/java/com/spring/app/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.spring.app.controller;
+
+import com.spring.app.form.UserForm;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    // 登録フォームを表示
+    @GetMapping("/register")
+    public String showRegisterForm(Model model) {
+        model.addAttribute("userForm", new UserForm());
+        return "register"; // templates/register.html を表示
+    }
+
+    // 登録フォーム送信時
+    @PostMapping("/register")
+    public String registerUser(
+            @Valid @ModelAttribute("userForm") UserForm userForm,
+            BindingResult bindingResult,
+            Model model) {
+
+        // バリデーションエラーがあれば戻す
+        if (bindingResult.hasErrors()) {
+            return "register";
+        }
+
+        // TODO: ユーザーのDB保存処理をここで行う
+
+        // TODO: セッションにログイン状態を保存する
+
+        // 登録後、TOPページ（仮）へ遷移
+        return "redirect:/top";
+    }
+}
