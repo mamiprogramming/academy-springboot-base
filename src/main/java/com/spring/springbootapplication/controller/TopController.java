2b36a5a19ad42822f0fc9c9cc76437d5e03e15d7
mamiprogramming <mamiprogramming@gmail.com>
2025-07-02 06:05:40 +0900

    未ログインならログイン画面にリダイレクト

--- src/main/java/com/spring/springbootapplication/controller/TopController.java
+++ src/main/java/com/spring/springbootapplication/controller/TopController.java
@@ -11,6 +11,13 @@ public class TopController {
     @GetMapping("/top")
     public String showTopPage(Model model, HttpSession session) {
         Object loginUser = session.getAttribute("loginUser");
+
+        if (loginUser == null) {
+            // 未ログインの場合はログイン画面へリダイレクト
+            return "redirect:/login";
+        }
+
+        // ログイン済みなら画面表示
         model.addAttribute("loginUser", loginUser);
         model.addAttribute("isLoginPage", false);
         return "top"; // resources/templates/top.html をレンダリング
