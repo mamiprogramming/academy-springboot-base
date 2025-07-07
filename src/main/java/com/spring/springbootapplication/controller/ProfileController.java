da6c6a65d438b71002c3f756ea91af813b4ffe2d
mamiprogramming <mamiprogramming@gmail.com>
2025-07-07 22:07:25 +0900

    学習チャート編集ページ（簡易）を作成・遷移

--- src/main/java/com/spring/springbootapplication/controller/ProfileController.java
+++ src/main/java/com/spring/springbootapplication/controller/ProfileController.java
@@ -9,6 +9,12 @@ public class ProfileController {
     // 簡易版自己紹介編集ページ表示
     @GetMapping("/profile/edit")
     public String showSimpleEdit() {
-        return "profile_edit"; // テンプレート名（HTMLファイル名から拡張子を除いたもの）
+        return "profile_edit"; // templates/profile_edit.html
+    }
+
+    // 簡易版スキル編集ページ表示
+    @GetMapping("/skill/edit")
+    public String showSkillEdit() {
+        return "skill_edit"; // templates/skill_edit.html
     }
 }
